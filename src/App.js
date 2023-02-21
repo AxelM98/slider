7af3f7d46afe4c8d87528d7fb95665c698b13128
Axel Magnusson <axelmagnusson1998@gmail.com>
2023-02-21 10:23:31 +0100

    commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import "./app.css";
+import { useState } from "react";
+import WestOutlinedIcon from "@mui/icons-material/WestOutlined";
+import EastOutlinedIcon from "@mui/icons-material/EastOutlined";
 
 function App() {
+  const [slide, setSlide] = useState(0);
+
+  const images = [
+    "https://images.pexels.com/photos/2559941/pexels-photo-2559941.jpeg",
+    "https://images.pexels.com/photos/3075993/pexels-photo-3075993.jpeg",
+    "https://images.pexels.com/photos/1408221/pexels-photo-1408221.jpeg",
+    "https://images.pexels.com/photos/15286/pexels-photo.jpg",
+  ];
+
+  const prevSlide = () => {
+    setSlide(slide === 0 ? 3 : (prev) => prev - 1);
+  };
+
+  const nextSlide = () => {
+    setSlide(slide === 3 ? 0 : (prev) => prev + 1);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="imgContainer" style={{transform: `translateX(-${slide * 100}vw)`}}>
+        <img src={images[0]} />
+        <img src={images[1]} />
+        <img src={images[2]} />
+        <img src={images[3]} />
+      </div>
+      <div className="icons">
+        <div className="icon icon_left" onClick={prevSlide}>
+          <WestOutlinedIcon style={{fontSize: "50"}}/>
+        </div>
+        <div className="icon icon_right" onClick={nextSlide}>
+          <EastOutlinedIcon style={{fontSize: "50"}}/>
+        </div>
+      </div>
     </div>
   );
 }
